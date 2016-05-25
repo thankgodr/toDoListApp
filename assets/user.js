8870d1dee257a0570b0c0a165aea933908dcedb1
ThankGod Richard <madueketf@gmail.com>
2016-05-25 13:30:29 +0100

    files added

---
+++ assets/user.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+  var ref = new Firebase("https://todolistapp23.firebaseio.com/");
+  var usersRef = ref.child("users");
+  $('.form-google-button').click(function(){
+    ref.authWithOAuthPopup("google", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData.google.cachedUserProfile);
+      usersRef.child(authData.uid).set({
+        details: {
+          gender: authData.google.cachedUserProfile.gender,
+          full_name: authData.google.cachedUserProfile.name,
+          picture: authData.google.cachedUserProfile.picture,
+        }
+      });
+      window.location = "index.html";
+
+    }
+  });
+  });
+
+  $('.form-facebook-button').click(function(){
+    ref.authWithOAuthPopup("facebook", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+  });
+
+  $('.form-twitter-button').click(function(){
+    ref.authWithOAuthPopup("twitter", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+  });
+})  
