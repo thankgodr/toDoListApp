e605eb417ba4bc0a082b43a4e4e24592507e036d
ThankGod Richard <madueketf@gmail.com>
2016-05-26 10:49:21 +0100

    reading from firebase and changed locations

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,67 +1,48 @@
+var ref = new Firebase("https://todolistapp23.firebaseio.com/");
+var authtoken = sessionStorage.getItem('token'),
+  uid;
+
+//Firebase Authentiate or reidrect back to logon page
+ref.auth(authtoken, function(error, result) {
+  if (error) {
+    console.log("Authentication Failed!", error);
+    window.location = "register.html";
+  } else {
+    uid = result.uid
+    var useref = ref.child("todo");
+    useref.child(uid).on("value", function(snapshot) {
+      for(key in snapshot.val()){
+        console.log(key);
+      }
+    }, function(errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+    console.log("Auth expires at:", new Date(result.expires * 1000));
+  }
+})
 
 jQuery(document).ready(function() {
-    //Firebase Authentiate or reidrect back to logon page
-	var ref = new Firebase("https://todolistapp23.firebaseio.com/");
-    var authtoken = sessionStorage.getItem('token');
-    var uid;
-    ref.auth(authtoken, function(error, result) {
-      if (error) {
-        console.log("Authentication Failed!", error);
-        window.location = "register.html";
-      } else {
-        uid = result.uid
-        console.log("Auth expires at:", new Date(result.expires * 1000));
-      }
-    })
-
-
-    /*
-        Fullscreen background
+  /*
+        Modals
     */
-    $.backstretch("assets/img/backgrounds/1.jpg");
-    
-    /*
-	    Modals
-	*/
-	$('.launch-modal').on('click', function(e){
-		e.preventDefault();
-		$( '#' + $(this).data('modal-id') ).modal();
-	});
-    
-    /*
-        Form validation
-   
-	$('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
-    	$(this).removeClass('input-error');
-    });
-    
-    $('.login-form').on('submit', function(e) {
-    	
-    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
-    		if( $(this).val() == "" ) {
-    			e.preventDefault();
-    			$(this).addClass('input-error');
-    		}
-    		else {
-    			$(this).removeClass('input-error');
-    		}
-    	});
-    	
-    });
-     */
+  $('.launch-modal').on('click', function(e) {
+    e.preventDefault();
+    $('#' + $(this).data('modal-id')).modal();
+  });
 
-    $('.login-form').on('submit', function() {
-        var name = $(this).find('input[name="toDoName"]').val();
-        var desc = $(this).find('input[name="form-desc"]').val();
-        var date = $(this).find('input[name="form-date"]').val();
-        var time = $(this).find('input[name="form-time"]').val();
-        var refTodo = ref.child("todo");
-        refTodo.child(uid).set({ uid : {
-           'name': name,
-           'desc': desc,
-           'date': date,
-           'time': time
-        }})
+  $('.login-form').on('submit', function() {
+    var timeInMs = Date.now();
+    var name = $(this).find('input[name="toDoName"]').val();
+    var desc = $(this).find('input[name="form-desc"]').val();
+    var date = $(this).find('input[name="form-date"]').val();
+    var time = $(this).find('input[name="form-time"]').val();
+    var refTodo = ref.child("todo");
+    refTodo.child(uid).child(timeInMs).set({
+      'name': name,
+      'desc': desc,
+      'date': date,
+      'time': time
     });
+  });
 
- });
+});
